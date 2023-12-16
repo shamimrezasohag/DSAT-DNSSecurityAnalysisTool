7c2126941fed20fd2a8234a533be8f4fefc37c96
A. S. M. Shamim Reza <sohag.shamim@gmail.com>
2023-12-16 11:14:36 +0600

    Added progress bar feature to DNS Security Analysis Tool

--- dns_security_analysis_tool.py
+++ dns_security_analysis_tool.py
@@ -3,6 +3,7 @@ import dns.reversename
 import argparse
 import logging
 import pandas as pd
+from tqdm import tqdm
 
 # Setup logging
 logging.basicConfig(filename='dns_security_audit.log', level=logging.INFO,
@@ -18,22 +19,16 @@ class DNSQueryTool:
         results = {'domain': domain}
         for record_type in record_types:
             results[record_type], results[f'{record_type}_response_code'] = self.query_record(domain, record_type)
-        results['PTR'], _ = self.check_reverse_dns(results.get('A', "") + results.get('AAAA', ""))
+        results['PTR'], _ = self.check_reverse_dns(results.get('A', '') + results.get('AAAA', ''))
         results['Open Resolver'] = self.check_open_resolver(domain)
         results['DNSSEC'] = self.check_dnssec(domain)
-        results['Anomalies'] = self.analyze_records(domain, results)
+        results['Anomalies'] = self.analyze_records(results)
         return results
 
     def query_record(self, domain, record_type):
         try:
             answers = self.resolver.resolve(domain, record_type)
             return "; ".join([answer.to_text() for answer in answers]), "NOERROR"
-        except dns.resolver.NoAnswer:
-            return "", "NOANSWER"
-        except dns.resolver.NXDOMAIN:
-            return "", "NXDOMAIN"
-        except dns.resolver.Timeout:
-            return "", "TIMEOUT"
         except Exception as e:
             return "", str(e)
 
@@ -62,12 +57,14 @@ class DNSQueryTool:
         except Exception as e:
             return f"DNSSEC Not Verified: {e}"
 
-    def analyze_records(self, domain, records):
+    def analyze_records(self, records):
         anomalies = {}
+        # Insert anomaly detection logic here
+        # Example: Check for unusual MX records
         mx_records = records.get('MX', "")
         if 'suspicious' in mx_records:
             anomalies['MX'] = "Suspicious MX record found"
-        # Additional analyses as needed
+        # Additional anomaly checks can be added here
         return "; ".join([f"{k}: {v}" for k, v in anomalies.items()])
 
 def generate_report(all_data, output_format, output_filename):
@@ -83,9 +80,11 @@ def process_domains(domains, dns_server, output_format, output_filename):
     tool = DNSQueryTool(dns_server)
     all_results = []
 
-    for domain in domains:
-        dns_results = tool.query_all_records(domain)
-        all_results.append(dns_results)
+    with tqdm(total=len(domains), desc="Analyzing Domains") as pbar:
+        for domain in domains:
+            dns_results = tool.query_all_records(domain)
+            all_results.append(dns_results)
+            pbar.update(1)
 
     generate_report(all_results, output_format, output_filename)
 
